9509596e1fabd0b04bbaf7640f2fb840e1316d74
Thet Minn Htun <thetminnhtun92@gmail.com>
2019-04-30 00:19:51 +0630

    finished gold calulator

---
+++ app.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        ufont: true,
+        ps: [],
+        ys: [],
+        qs: [16, 15, 14, 13, 12],
+        form: {
+            k: null,
+            p: 0,
+            y: 0,
+            currentGoldPrice: null,
+            q: 16,
+        },
+        result: 0,
+    },
+    methods: {
+        cp() {
+            for (let i = 0; i < 17; i++) {
+                this.ps[i] = i;
+            }
+        },
+        cy() {
+            for (let i = 0; i < 9; i++) {
+                this.ys[i] = i;
+            }
+        },
+        calc() {
+            let k = this.form.k;
+            let p = this.form.p;
+            let y = this.form.y;
+            let price = this.form.currentGoldPrice;
+            let totalKyats = k + ( p / 16 ) + ( ( y / 8) / 16 );
+            let amount = ( this.form.q / 16 ) * price;
+            this.result = Math.floor(totalKyats * amount).toLocaleString();
+            this.form = { k: null, p: 0, y: 0, currentGoldPrice: null, q: 16 }
+        },
+        changeFont() {
+            this.ufont = !this.ufont;
+        }
+    },
+    created() {
+        this.cp(),
+        this.cy()
+    }
+})
